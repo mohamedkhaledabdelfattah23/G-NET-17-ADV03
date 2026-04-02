82a9359c7f8e30d7b4f7baf45eeaa3d8bc9affbb
mohamedkhaledabdelfattah23 <mohamedkhaledabdelfattah23@gmail.com>
2026-04-03 01:39:14 +0200

    Exercise 1

--- Advanced C# 03/Program.cs
+++ Advanced C# 03/Program.cs
@@ -4,7 +4,76 @@
     {
         static void Main(string[] args)
         {
+
+            /*
+             
+             
+             Exercise 1: Student Grade Manager
            
+                3. Sort the grades ascending, then print
+                4. Get the first grade above 90
+                5. Get all grades below 75 (failing grades)
+                6. Remove all failing grades (below 75)
+                7. Check if any grade equals 100
+                8. Create a List&lt;string&gt; where each grade becomes &quot;Grade: X&quot;
+             
+
+             
+             */
+
+
+            #region Exercise 1:
+            //List<int> grades = [85, 92, 78, 95, 88, 70, 100, 65];
+
+
+            //Console.WriteLine("Grades: " + string.Join(", ", grades));
+
+            //Console.WriteLine($"Count: {grades.Count}");
+
+            //Console.WriteLine($"First: {grades.First()}");
+
+            //Console.WriteLine($"Last: {grades.Last()}");
+
+            //grades.Sort();
+            //Console.WriteLine("Grades: " + string.Join(", ", grades));
+
+            //int first = grades.Find(x => x > 90);
+            //Console.WriteLine($"First > 90: {first}");
+
+
+            //List<int> gra = grades.FindAll(x => x < 75);
+
+            //Console.WriteLine("Grades below 75 : " + string.Join(", ", gra));
+
+
+
+            //grades.RemoveAll(g => g < 75);
+
+            //Console.WriteLine("\nAfter removing failing grades:");
+            //grades.ForEach(g => Console.Write(g + " "));
+
+
+            //bool has100 = grades.Contains(100);
+            //Console.WriteLine($"\nContains 100? {has100}");
+
+
+            //List<string> gradeStrings = grades
+            //    .Select(g => $"Grade: {g}")
+            //    .ToList();
+
+
+            //Console.WriteLine("Formatted Grades:");
+            //gradeStrings.ForEach(Console.WriteLine);
+
+
+            #endregion
+
+
+
+
+
+
+
         }
     }
 }
